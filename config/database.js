01e90301040e8f3d22f5e4c92d5c8deb17e4abe0
dpxrk <danielpark0503@gmail.com>
2021-02-02 11:20:50 -0500

    fixed the csrf bug

--- config/database.js
+++ config/database.js
@@ -1,26 +1,14 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+const {
+  db: { username, password, database, host },
+} = require('./index');
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
-module.exasd ports = router;
+module.exports = router;
