b1102db41eb1b82449afa9f01d365a0d13693f80
dpxrk <danielpark0503@gmail.com>
2021-02-04 13:05:33 -0500

    database.js changes

--- config/database.js
+++ config/database.js
@@ -9,6 +9,7 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
+    seederStoragee: 'sequelize'
   },
   production: {
     use_env_variable: 'postgres://gwgufwbuckcrfq:daf6e50a8d4778de6bf00590e1de3456bb7448e13bb7baf010406ae6d8aff1c8@ec2-54-90-13-87.compute-1.amazonaws.com:5432/da0tjmm246fvhs',
